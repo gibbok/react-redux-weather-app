d00d75f97d71be2aa7cb89ed43d3aa5f23d6069d
GibboK <gibbok.coding@gmail.com>
2017-03-25 16:23:08 +0100

    Add webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: './build',
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
