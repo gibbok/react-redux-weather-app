d75cd13fa1c63e3372172a363250907d9791c185
GibboK <gibbok.coding@gmail.com>
2017-03-26 11:05:19 +0200

    Add support for jsx in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
             }
