347ade91f14020b715124a4a441d74d37ae61cf4
GibboK <gibbok.coding@gmail.com>
2017-03-29 20:04:40 +0200

    Add basic index and resucer

---
+++ src/app/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const App = () => (
+    <div>
+        App
+    </div>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-﻿console.log('hello world');
+﻿import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './locationFinder/locationFinderReducer'
+import App from './app/App'
+
+const store = createStore(reducer)
+
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+
+
+console.log('hello world dcc');
