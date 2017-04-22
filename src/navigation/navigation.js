05b31552a93e80020993fb121dcfba87ff87b0b3
GibboK <gibbok.coding@gmail.com>
2017-04-22 10:53:03 +0200

    Add highlight active navigation

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -3,9 +3,10 @@ import React from 'react'
 const idSuffix = 'navigation__'
 
 const Navigation = ({ onClick, id, title, tooltip, isActive }) => (
+  /* replace b by css solution only later on */
   <li id={`${idSuffix}${id}`} onClick={onClick} alt={tooltip} data-active={isActive}>
-    {title}
-  </li>
+    {isActive ? <b>{title}</b> : title }
+  </li >
 )
 
 export default Navigation
