8c9f92966cb8096c17e4d01123d44748c7b53d27
GibboK <gibbok.coding@gmail.com>
2017-05-20 09:46:55 +0200

    Add cleanup code forecast hourly chart

--- src/forecastHourly/forecastHourlyChart.js
+++ src/forecastHourly/forecastHourlyChart.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, Tooltip } from 'recharts'
 
-/*
-[{"momentId":"2017:4:20:6","hour":9,"temp":288.03,"weatherIcon":"01n"},{"momentId":"2017:4:20:6","hour":12,"temp":288.83,"weatherIcon":"01d"},{"momentId":"2017:4:20:6","hour":15,"temp":290.74,"weatherIcon":"02d"},{"momentId":"2017:4:20:6","hour":18,"temp":292.973,"weatherIcon":"01d"},{"momentId":"2017:4:20:6","hour":21,"temp":292.783,"weatherIcon":"01d"}]
-
-*/
-
-//  const { payload: { name, main, weather, wind, sys, visibility } } = action
+// use icon from https://websygen.github.io/owfont/#usage
 
 const CustomizedDot = React.createClass({
   render () {
     const { cx, cy, payload: {weatherIcon} } = this.props
-    const styles = {
-      fontSize: '200px'
-    }
     return (
-      <svg x={cx - 10} y={cy - 10} width={100} height={100} viewBox='0 0 1024 1024'>
-        <foreignObject width='100' height='100'>
-          <div style={styles}> {weatherIcon}</div>
+      <svg x={cx - 10} y={cy + 20}>
+        <foreignObject width='100%' height='100%'>
+          <div>{weatherIcon}</div>
         </foreignObject>
       </svg>
     )
@@ -25,9 +17,9 @@ const CustomizedDot = React.createClass({
 })
 
 const ForecastHourlyChart = ({ forecastHourly }) => (
-  <LineChart width={500} height={250} data={forecastHourly}>
+  <LineChart width={500} height={250} data={forecastHourly} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
     <Line type='monotone' dataKey='temp' stroke='#8884d8' dot={<CustomizedDot />} />
-    <CartesianGrid stroke='#ccc' />
+    <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
     <XAxis dataKey='hour' />
     <Tooltip />
   </LineChart>
