a0a715a5936f4e49356552446439e132d156e9bb
GibboK <gibbok.coding@gmail.com>
2017-05-21 14:45:42 +0200

    Add refatory separate module for CustomizedDot

--- src/forecastHourly/forecastHourlyChart.js
+++ src/forecastHourly/forecastHourlyChart.js
@@ -1,19 +1,6 @@
 import React from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, Tooltip } from 'recharts'
-import IconWeather from '../shared/IconWeather'
-
-const CustomizedDot = React.createClass({
-  render () {
-    const { cx, cy, payload: {weatherIconCode} } = this.props
-    return (
-      <svg x={cx - 10} y={cy + 20}>
-        <foreignObject width='100%' height='100%'>
-          <IconWeather code={weatherIconCode} />
-        </foreignObject>
-      </svg>
-    )
-  }
-})
+import CustomizedDot from './CustomizedDot'
 
 const ForecastHourlyChart = ({ forecastHourly }) => (
   <LineChart width={500} height={250} data={forecastHourly} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
