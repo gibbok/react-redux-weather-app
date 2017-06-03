c614bf2454c988c368c432f2d881c40fdad40a72
GibboK <gibbok.coding@gmail.com>
2017-06-03 11:29:51 +0200

    Add basic routes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './app/store'
 import App from './app/App'
+import Root from './app/Root'
 
-render(
+/* render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
+) */
+
+render(
+  <Root store={store} />,
+    document.getElementById('root')
 )
