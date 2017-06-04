bf391fc7932c11be46b0c7ee91aaca99c9690961
GibboK <gibbok.coding@gmail.com>
2017-06-04 11:32:23 +0200

    Add Map rendering

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import * as ol from 'openlayers'
+import styles from '../../node_modules/openlayers/css/ol.css'
 
-const Map = () => (
-  <h1>Map</h1>
-)
+class Map extends React.Component {
+  componentDidMount () {
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 4
+      })
+    })
+    var layerCloud = new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: 'http://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=9f585babc59bf435c42319b4e9b69766'
+      })
+    })
+    map.addLayer(layerCloud)
+  }
+  render () {
+    return <div id='map' className='map' />
+  }
+}
 
 export default Map
