845b870bb1f50a842779029596488cf786161b1f
GibboK <gibbok.coding@gmail.com>
2017-09-05 21:08:03 +0200

    Add change unit to celsius

--- src/app/api.js
+++ src/app/api.js
@@ -2,11 +2,11 @@ import {apiUrl, apiKey, apiMapTemperature, apiMapPrecipitation, apiMapPressure, 
 
 export const find = query => `${apiUrl}find?q=${query}&APPID=${apiKey}`
 
-export const weather = id => `${apiUrl}weather?id=${id}&APPID=${apiKey}`
+export const weather = id => `${apiUrl}weather?id=${id}&APPID=${apiKey}&units=metric`
 
-export const forecastDaily = id => `${apiUrl}forecast/daily?id=${id}&APPID=${apiKey}`
+export const forecastDaily = id => `${apiUrl}forecast/daily?id=${id}&APPID=${apiKey}&units=metric`
 
-export const forecast = id => `${apiUrl}forecast?id=${id}&APPID=${apiKey}`
+export const forecast = id => `${apiUrl}forecast?id=${id}&APPID=${apiKey}&units=metric`
 
 export const mapTemperature = () => `${apiMapTemperature}?APPID=${apiKey}`
 
