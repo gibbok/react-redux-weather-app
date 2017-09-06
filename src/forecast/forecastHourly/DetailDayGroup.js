8a73bea428af8dff1ae7cbed3a565cd644cd4e53
GibboK <gibbok.coding@gmail.com>
2017-09-06 10:53:44 +0200

    Add goup rendeing day and hours

--- src/forecast/forecastHourly/DetailDayGroup.js
+++ src/forecast/forecastHourly/DetailDayGroup.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import moment from 'moment'
+import Detail from './Detail'
 
-const DetailDayGroup = ({ day }) => {
-  debugger
-  const dayFormat = moment.unix(day.timespan).format('ddd, MMM D')
+const DetailDayGroup = ({ timespan, infos }) => {
+  const dayFormat = moment.unix(timespan).format('ddd, MMM D')
   return (
     <div>
-      {dayFormat}
+      <div>
+        {dayFormat}
+      </div>
+      <div>
+        {infos.map((day, index) =>
+          <Detail
+            key={index}
+            {...infos[index]}
+       />
+     )}
+      </div>
     </div>
   )
 }
--- src/forecast/forecastHourly/Details.js
+++ src/forecast/forecastHourly/Details.js
@@ -1,43 +1,24 @@
 import React from 'react'
-// import Detail from './Detail'
 import DetailDayGroup from './DetailDayGroup'
 
-/*
- {
-  date1: [],
-  date2: []
- }
-*/
-
 const Details = ({ forecastHourly }) => {
-  let result = forecastHourly.reduce((acc, item) => {
+  const dayGroup = forecastHourly.reduce((acc, item) => {
     (acc[item.momentId] || (acc[item.momentId] = [])).push(item)
     return acc
   }, {})
-  console.log(result)
+  const dayGroupRender = Object.keys(dayGroup).map((day, index) => {
+    const infos = dayGroup[day]
+    return <DetailDayGroup
+      key={index}
+      timespan={dayGroup[day][0].timespan}
+      infos={infos}
+/>
+  })
   return (
     <div>
-      {forecastHourly.map((day, index) =>
-        <DetailDayGroup
-          key={index}
-          day={day}
-      />
-    )}
+      {dayGroupRender}
     </div>
   )
 }
-
-// const Details = ({ forecastHourly }) => (
-//   <div>
-//     <ul>
-//       {forecastHourly.map((day, index) =>
-//         <Detail
-//           key={index}
-//           {...forecastHourly[index]}
-//       />
-//     )}
-//     </ul>
-//   </div>
-// )
 
 export default Details
