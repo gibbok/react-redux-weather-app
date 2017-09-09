589df8a55c8c18650a22b51620980e67b9dc8c55
GibboK <gibbok.coding@gmail.com>
2017-09-09 16:16:12 +0200

    Add a separation of <Location /> per test case bases

--- src/forecast/locationFinder/Location.test.js
+++ src/forecast/locationFinder/Location.test.js
@@ -1,25 +1,37 @@
 import React from 'react'
-// import { shallow, mount, render } from 'enzyme'
 import { render, mount, shallow } from 'enzyme'
 
 import Location from './Location'
 
 describe('<Location />', () => {
-  const location = <Location
-    id={3067696}
-    name='Prague'
-    country='CZ'
-    />
   it('should render', () => {
-    // use `onClick={undefined}` because `onLocationClick` property is not defined in `<Location />`
-    expect(shallow(location).contains(<li><a onClick={undefined}>Prague, CZ</a></li>)).toBe(true)
+    expect(shallow(
+      <Location
+        id={3067696}
+        name='Prague'
+        country='CZ'
+        onLocationClick='test'
+      />).contains(<li><a onClick='test'>Prague, CZ</a></li>)).toBe(true)
   })
 
   it('should render to static HTML', function () {
-    expect(render(location).text()).toEqual(`Prague, CZ`)
+    expect(render(
+      <Location
+        id={3067696}
+        name='Prague'
+        country='CZ'
+        onLocationClick='test'
+      />).text()).toEqual(`Prague, CZ`)
   })
 
   it('should mount in a full DOM', function () {
-    expect(mount(location).find('li > a').length).toBe(1)
+    expect(mount(
+      <Location
+        id={3067696}
+        name='Prague'
+        country='CZ'
+        onLocationClick={() => {}}
+      />
+    ).find('li > a').length).toBe(1)
   })
 })
