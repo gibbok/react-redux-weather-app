db62a9c2a9246e1d454b96da6a101fd5c12ba39d
GibboK <gibbok.coding@gmail.com>
2017-09-09 22:54:26 +0200

    Add refactory on separated test cases

--- src/forecast/locationFinder/LocationFinderSearch.test.js
+++ src/forecast/locationFinder/LocationFinderSearch.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { render, mount, shallow } from 'enzyme'
 import { mount, shallow } from 'enzyme'
 import LocationFinderSearch from './LocationFinderSearch'
 import sinon from 'sinon'
@@ -14,19 +13,23 @@ describe('<LocationFinderSearch />', () => {
   it('should accept change', () => {
     // given
     const change = sinon.spy()
-    const click = sinon.spy()
     const actualNode = shallow(
       <LocationFinderSearch
         onLocationChange={change}
-        onSearchClick={click}
     />)
-
     // when
     actualNode.find('input[type=\'text\']').simulate('change')
-    actualNode.find('input[type=\'submit\']').simulate('click')
-
     // then
     sinon.assert.called(change)
+  })
+
+  it('should accept click on submit', () => {
+    const click = sinon.spy()
+    const actualNode = shallow(
+      <LocationFinderSearch
+        onSearchClick={click}
+    />)
+    actualNode.find('input[type=\'submit\']').simulate('click')
     sinon.assert.called(click)
   })
 })
