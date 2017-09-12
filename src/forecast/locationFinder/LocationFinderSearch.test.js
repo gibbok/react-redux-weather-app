6c4ffc6b386fbddc3775131db423fe0cd85b6723
GibboK <gibbok.coding@gmail.com>
2017-09-12 08:19:04 +0200

    Add test case LocationFinderSearch it should render

--- src/forecast/locationFinder/LocationFinderSearch.test.js
+++ src/forecast/locationFinder/LocationFinderSearch.test.js
@@ -2,8 +2,18 @@ import React from 'react'
 import { mount, shallow } from 'enzyme'
 import LocationFinderSearch from './LocationFinderSearch'
 import sinon from 'sinon'
+import toJson from 'enzyme-to-json'
 
 describe('<LocationFinderSearch />', () => {
+  it('should render', () => {
+    const wrapper = shallow(
+      <LocationFinderSearch
+        onLocationChange={() => {}}
+      />
+      )
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
   it('should mount in a full DOM', () => {
     expect(mount(<LocationFinderSearch />).find('form').length).toBe(1)
     expect(mount(<LocationFinderSearch />).find('input[type=\'text\']').length).toBe(1)
