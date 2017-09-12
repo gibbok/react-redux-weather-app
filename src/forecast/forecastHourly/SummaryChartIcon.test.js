b773795371572866619eb5e60922c74e90364dc9
GibboK <gibbok.coding@gmail.com>
2017-09-12 12:09:48 +0200

    Add test case SummaryChartIcon

---
+++ src/forecast/forecastHourly/SummaryChartIcon.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import toJson from 'enzyme-to-json'
+import SummaryChartIcon from './SummaryChartIcon'
+
+describe('<SummaryChartIcon />', () => {
+  it('should render', () => {
+    const wrapper = shallow(
+      <SummaryChartIcon
+        payload={{weatherIconCode: 200}} />
+      )
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
