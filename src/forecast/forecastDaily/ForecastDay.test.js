115d13e5e25de1456cd52fd6ee2e7a3888ee44e1
GibboK <gibbok.coding@gmail.com>
2017-09-12 08:02:28 +0200

    Add enzyme test case find IconWeather in ForecastDay

--- src/forecast/forecastDaily/ForecastDay.test.js
+++ src/forecast/forecastDaily/ForecastDay.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import ForecastDay from './ForecastDay'
 import toJson from 'enzyme-to-json'
+import IconWeather from '../../shared/icon/IconWeather'
 
 describe('<ForecastDay />', () => {
   it('should render', () => {
@@ -16,5 +17,19 @@ describe('<ForecastDay />', () => {
       />
     )
     expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('should contain a IconWeather component', () => {
+    const wrapper = shallow(
+      <ForecastDay
+        onClick={() => {}}
+        date={1504811633838}
+        tempMin={10}
+        tempMax={15}
+        iconCode={200}
+        weatherDescription={'storm showers'}
+        />
+      )
+    expect(wrapper.find(IconWeather).length).toBe(1)
   })
 })
