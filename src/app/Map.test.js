8adf3029d450455c209f894ca72de9cd16224ca1
GibboK <gibbok.coding@gmail.com>
2017-09-13 07:54:43 +0200

    Add test case for map

---
+++ src/app/Map.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Map from './Map'
+
+describe('<Map />', () => {
+  it('should render', () => {
+    const wrapper = shallow(<Map />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
