d95c78b0c79784ea696198326986533b73eab7e2
GibboK <gibbok.coding@gmail.com>
2017-09-14 20:35:42 +0200

    Add test cases

--- src/shared/navigation/Navigation.test.js
+++ src/shared/navigation/Navigation.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import Navigation from './Navigation'
+import toJson from 'enzyme-to-json'
 import { NavLink } from 'react-router-dom'
 
 describe('<Navigation />', () => {
+  it('should render with snapshot', () => {
+    const wrapper = shallow(
+      <Navigation
+        onClick={'test'}
+        id={1}
+        title={'test title'}
+        tooltip={'test tooltop'}
+        url={'/map'}
+       />
+      )
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
   it('should render', () => {
     const wrapper = shallow(
       <Navigation
