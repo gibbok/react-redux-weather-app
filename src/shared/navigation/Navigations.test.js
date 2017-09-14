f6b26e6462730c34012c2598c83ef956e2f4e631
GibboK <gibbok.coding@gmail.com>
2017-09-14 18:02:06 +0200

    Add test cases

--- src/shared/navigation/Navigations.test.js
+++ src/shared/navigation/Navigations.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {shallow} from 'enzyme'
+import toJson from 'enzyme-to-json'
 import Navigations from './Navigations'
 import Navigation from './Navigation'
 
 describe('<Navigations />', () => {
-  it('should render two <Navigation /> components', () => {
-    const data = [
+  it('should render', () => {
+    const dataState = [
       {
         id: 'home',
         title: 'Weather',
@@ -23,7 +24,32 @@ describe('<Navigations />', () => {
     ]
     const wrapper = shallow(
       <Navigations
-        navigations={data}
+        navigations={dataState}
+      />
+    )
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('should render two <Navigation /> components', () => {
+    const dataState = [
+      {
+        id: 'home',
+        title: 'Weather',
+        tooltip: 'Weather',
+        isActive: true,
+        url: '/'
+      },
+      {
+        id: 'map',
+        title: 'Map',
+        tooltip: 'Weather map',
+        isActive: false,
+        url: '/map'
+      }
+    ]
+    const wrapper = shallow(
+      <Navigations
+        navigations={dataState}
       />
     )
     expect(wrapper.find(Navigation).length).toBe(2)
