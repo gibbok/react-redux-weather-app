f35cb18e07926ebe05d96090e50486ce6c199352
GibboK <gibbok.coding@gmail.com>
2017-09-14 11:19:15 +0200

    Add test case dispatch to ForecastHourlyContainer

--- src/forecast/forecastHourly/ForecastHourlyContainer.test.js
+++ src/forecast/forecastHourly/ForecastHourlyContainer.test.js
@@ -4,7 +4,7 @@ import {shallow} from 'enzyme'
 import toJson from 'enzyme-to-json'
 import configureStore from 'redux-mock-store'
 import ForecastHourlyContainer from './ForecastHourlyContainer'
-import {getForecastHourly} from './forecastHourlyActions'
+import {getForecastHourly, setForecastHourlyActiveReportType} from './forecastHourlyActions'
 import location from '../../app/location'
 import { createMomentId } from '../../app/utility'
 
@@ -30,10 +30,30 @@ describe('<ForecastHourlyContainer />', () => {
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('should dispatch', () => {
+  it('should dispatch action getForecastHourly', () => {
     const state = 'state'
     const store = createMockStore(state)
     const action = getForecastHourly(location().locationId)
+
+    store.dispatch(action)
+    expect(store.getAction(action.type)).toEqual(action)
+    expect(store.getActions()).toEqual([action])
+    expect(store.isActionDispatched(action)).toBe(true)
+    expect(store.isActionTypeDispatched(action.type)).toBe(true)
+    expect(store.getState()).toBe(state)
+
+    const dispatchMock = createMockDispatch()
+    dispatchMock.dispatch(action)
+    expect(dispatchMock.getAction(action.type)).toEqual(action)
+    expect(dispatchMock.getActions()).toEqual([action])
+    expect(dispatchMock.isActionDispatched(action)).toBe(true)
+    expect(dispatchMock.isActionTypeDispatched(action.type)).toBe(true)
+  })
+
+  it('should dispatch action setForecastHourlyActiveReportType', () => {
+    const state = 'state'
+    const store = createMockStore(state)
+    const action = setForecastHourlyActiveReportType('details')
 
     store.dispatch(action)
     expect(store.getAction(action.type)).toEqual(action)
