17651e3663d99e7b47a12de18f0eb17678785506
GibboK <gibbok.coding@gmail.com>
2017-09-15 08:51:49 +0200

    Add test cases using sinon

--- src/shared/navigation/Navigations.test.js
+++ src/shared/navigation/Navigations.test.js
@@ -3,6 +3,7 @@ import {shallow} from 'enzyme'
 import toJson from 'enzyme-to-json'
 import Navigations from './Navigations'
 import Navigation from './Navigation'
+import sinon from 'sinon'
 
 describe('<Navigations />', () => {
   it('should render', () => {
@@ -53,5 +54,33 @@ describe('<Navigations />', () => {
       />
     )
     expect(wrapper.find(Navigation).length).toBe(2)
+  })
+
+  it('should simulate click event', () => {
+    const dataState = [
+      {
+        id: 'home',
+        title: 'Weather',
+        tooltip: 'Weather',
+        isActive: true,
+        url: '/'
+      },
+      {
+        id: 'map',
+        title: 'Map',
+        tooltip: 'Weather map',
+        isActive: false,
+        url: '/map'
+      }
+    ]
+    const onNavigationClick = sinon.spy()
+    const wrapper = shallow(
+      <Navigations
+        navigations={dataState}
+        onNavigationClick={onNavigationClick}
+      />
+    )
+    wrapper.find({ id: 'map' }).simulate('click')
+    expect(onNavigationClick.called).toEqual(true)
   })
 })
