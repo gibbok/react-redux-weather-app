47576df5356491e726ad8b4c91a0396ef8f3ca78
GibboK <gibbok.coding@gmail.com>
2017-09-15 14:29:44 +0200

    Add test case check asynch actions This test case checks if an asych action can be dispatched and if the returned data match in first level deep properties as in the API counterpart

--- src/forecast/forecastDaily/forecastDailyActions.test.js
+++ src/forecast/forecastDaily/forecastDailyActions.test.js
@@ -1,9 +1,48 @@
 import {getForecastDaily} from './forecastDailyActions'
+import * as types from './forecastDailyActionTypes'
+import nock from 'nock'
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
 
 describe('forecastDailyActions', () => {
-  it('should have a type of GET_FORECAST_DAILY', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  it('should create an action getForecastDaily', () => {
+    const expectedAction = {
+      type: types.GET_FORECAST_DAILY,
+      payload: new Promise(() => {}, () => {})
+    }
+    expect(getForecastDaily(5128581)).toEqual(expectedAction)
+  })
+
+  it('should create action GET_FORECAST_DAILY', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore()
     const action = getForecastDaily(5128581)
-    expect(action.type).toEqual('GET_FORECAST_DAILY')
-    expect(action.payload).toEqual(new Promise(() => {}, () => {}))
+    const expectedAction = [
+      { type: types.GET_FORECAST_DAILY, payload: new Promise(() => {}, () => {}) }
+    ]
+
+    store.dispatch(action)
+    expect(store.getActions()).toEqual(expectedAction)
+  })
+
+  it('should get valid api data when GET_FORECAST_DAILY has been fulfilled', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore()
+    const action = getForecastDaily(5128581)
+    store.dispatch(action).payload.then(data => {
+      expect(data).toEqual(expect.objectContaining({
+        city: expect.any(Object),
+        cod: expect.any(String),
+        message: expect.any(Number),
+        cnt: expect.any(Number),
+        list: expect.any(Array)
+      }))
+    })
   })
 })
