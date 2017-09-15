cf6c580977fdd7a31be09591dd73c71dc2a8a744
GibboK <gibbok.coding@gmail.com>
2017-09-15 19:45:33 +0200

    Remove deprecated code

--- src/forecast/forecastDaily/forecastDailyActions.test.js
+++ src/forecast/forecastDaily/forecastDailyActions.test.js
@@ -9,49 +9,6 @@ const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
 
 describe('forecastDailyActions', () => {
-  // afterEach(() => {
-  //   nock.cleanAll()
-  // })
-
-  // it('should create an action getForecastDaily', () => {
-  //   const expectedAction = {
-  //     type: types.GET_FORECAST_DAILY,
-  //     payload: new Promise(() => {}, () => {})
-  //   }
-  //   expect(getForecastDaily(5128581)).toEqual(expectedAction)
-  // })
-
-  // deprecated because now we are using a mock for fetch
-  // it('should create action GET_FORECAST_DAILY', () => {
-  //   const middlewares = [thunk]
-  //   const mockStore = configureMockStore(middlewares)
-  //   const store = mockStore()
-  //   const action = getForecastDaily(5128581)
-  //   const expectedAction = [
-  //     { type: types.GET_FORECAST_DAILY, payload: new Promise(() => {}, () => {}) }
-  //   ]
-
-  //   store.dispatch(action)
-  //   expect(store.getActions()).toEqual(expectedAction)
-  // })
-
-  // refactory this code at api.test.js level
-  // it('should get valid api data when GET_FORECAST_DAILY has been fulfilled', () => {
-  //   const middlewares = [thunk]
-  //   const mockStore = configureMockStore(middlewares)
-  //   const store = mockStore()
-  //   const action = getForecastDaily(5128581)
-  //   store.dispatch(action).payload.then(data => {
-  //     expect(data).toEqual(expect.objectContaining({
-  //       city: expect.any(Object),
-  //       cod: expect.any(String),
-  //       message: expect.any(Number),
-  //       cnt: expect.any(Number),
-  //       list: expect.any(Array)
-  //     }))
-  //   })
-  // })
-
   it('should dispatch action GET_FORECAST_DAILY and check for its fulfillment', () => {
     const location = 5128581
     const store = mockStore({ })
