27b18463b8e9e73f99fc0a703eb10c7474a75f0b
GibboK <gibbok.coding@gmail.com>
2017-09-15 11:38:57 +0200

    Add test case full coverage for LocationFinder

--- src/forecast/locationFinder/LocationFinder.test.js
+++ src/forecast/locationFinder/LocationFinder.test.js
@@ -3,8 +3,35 @@ import { shallow } from 'enzyme'
 import LocationFinder from './LocationFinder'
 import Location from './Location'
 import toJson from 'enzyme-to-json'
+import sinon from 'sinon'
 
 describe('<LocationFinder />', () => {
+  it('should click event', () => {
+    const data = [
+      {
+        country: 'CZ',
+        id: 3067696,
+        name: 'Prague'
+      },
+      {
+        country: 'US',
+        id: 4548393,
+        name: 'Prague'
+      }
+    ]
+    const onLocationClick = sinon.spy()
+    const wrapper = shallow(
+      <LocationFinder
+        locations={data}
+        onLocationClick={onLocationClick}
+    />)
+    const dom = wrapper.find({ id: 3067696 }).dive().find('li > a')
+    expect(dom.length).toBe(1)
+
+    dom.simulate('click')
+    expect(onLocationClick.called).toEqual(true)
+  })
+
   it('should render two <Location /> components', () => {
     const data = [
       {
