600e5744728d622ff4ad4d0637cac6cc78e4c0ed
GibboK <gibbok.coding@gmail.com>
2017-09-18 17:49:49 +0200

    Add flow notation for location

--- src/app/location.js
+++ src/app/location.js
@@ -1,4 +1,5 @@
-const location = () => {
+// @flow
+const location = ():{locationId: number, geo: Array<number>, isFetching:boolean} => {
   return {
     locationId: 5128581, // nyc usa
     geo: [-74.006, 40.7143],
