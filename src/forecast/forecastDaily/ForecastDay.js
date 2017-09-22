345808af57d4c36409d6d9eb0ba05075ba25cf28
GibboK <gibbok.coding@gmail.com>
2017-09-22 13:15:40 +0200

    Add flow annotation for ForecastDay

--- src/forecast/forecastDaily/ForecastDay.js
+++ src/forecast/forecastDaily/ForecastDay.js
@@ -1,8 +1,9 @@
+// @flow
 import React from 'react'
 import moment from 'moment'
 import IconWeather from '../../shared/icon/IconWeather'
 
-const ForecastDay = ({ date, tempMin, tempMax, iconCode, weatherDescription }) => {
+const ForecastDay = ({ date, tempMin, tempMax, iconCode, weatherDescription }:{+date:Date, +tempMin: number, +tempMax: number, +iconCode:string, +weatherDescription:string }) => {
   const dateFormat = moment.unix(date).format('ddd, MMM D')
   const tempMinRounded = Math.round(tempMin)
   const tempMaxRounded = Math.round(tempMax)
