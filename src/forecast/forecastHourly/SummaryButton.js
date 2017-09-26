ed16d09a4533bf72f604451f6a393494d80a09ab
GibboK <gibbok.coding@gmail.com>
2017-09-26 10:36:36 +0200

    Add flow annotation for SummaryButton (+1 squashed commits)
    Squashed commits:
    
    [7eb44b4] Add flow annotation for SummaryButton

--- src/forecast/forecastHourly/SummaryButton.js
+++ src/forecast/forecastHourly/SummaryButton.js
@@ -1,6 +1,13 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 
-const SummaryButton = ({ onSummaryClick }) => {
+/* eslint-disable no-undef */
+type PropsType = {
+  +onSummaryClick: Function
+}
+/* eslint-enable no-undef */
+
+const SummaryButton:React.StatelessFunctionalComponent<any> = ({ onSummaryClick }:PropsType):React.Element<any> => {
   return (
     <button type='button' onClick={(e) => onSummaryClick(e)}>Summary</button>
   )
