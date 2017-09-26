6d0f2a61e5db84a4b83d5c37735d5bfeb6a6d017
GibboK <gibbok.coding@gmail.com>
2017-09-26 10:17:01 +0200

    Add flow annotation for DetailsButton

--- src/forecast/forecastHourly/DetailsButton.js
+++ src/forecast/forecastHourly/DetailsButton.js
@@ -1,6 +1,14 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 
-const DetailsButton = ({ onDetailsClick }) => {
+/* eslint-disable no-undef */
+type PropsType = {
+  onDetailsClick: Function,
+}
+
+/* eslint-enable no-undef */
+
+const DetailsButton:React.StatelessFunctionalComponent<any> = ({ onDetailsClick }:PropsType):React.Element<any> => {
   return (
     <button type='button' onClick={(e) => onDetailsClick(e)}>Details</button>
   )
