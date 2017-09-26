8c537ba8974ad7e7bf31fa34ff7fa363399731b1
GibboK <gibbok.coding@gmail.com>
2017-09-26 11:22:35 +0200

    Add flow annotation for Location

--- src/forecast/locationFinder/Location.js
+++ src/forecast/locationFinder/Location.js
@@ -1,6 +1,17 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 
-const Location = ({ onLocationClick, id, name, country }) => (
+/* eslint-disable no-undef */
+type PropsType = {
+  onLocationClick: Function,
+  id: number,
+  name: string,
+  country:string
+}
+
+/* eslint-enable no-undef */
+
+const Location:React.StatelessFunctionalComponent<any> = ({ onLocationClick, id, name, country }:PropsType):React.Element<any> => (
   <li>
     <a onClick={onLocationClick}>{name}, {country}</a>
   </li>
