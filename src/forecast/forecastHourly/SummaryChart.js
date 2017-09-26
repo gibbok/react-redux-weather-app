f3edf272b36a437bd40c2c75b45c6e57b4295386
GibboK <gibbok.coding@gmail.com>
2017-09-26 10:50:46 +0200

    Add flow annotation SummaryChart

--- src/forecast/forecastHourly/SummaryChart.js
+++ src/forecast/forecastHourly/SummaryChart.js
@@ -1,10 +1,18 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
+import {ForecastHourlyType} from '../../types' // eslint-disable-line no-unused-vars
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import SummaryChartIcon from './SummaryChartIcon'
 import CustomTooltip from './CustomTooltip'
 import moment from 'moment'
 
-const SummaryChart = ({ forecastHourly }) => {
+/* eslint-disable no-undef */
+type PropsType = {
+  +forecastHourly: ForecastHourlyType
+}
+/* eslint-enable no-undef */
+
+const SummaryChart:React.StatelessFunctionalComponent<any> = ({ forecastHourly }:PropsType):React.Element<any> => {
   let forecastHourlyFormat = forecastHourly.map(data => {
     return {
       momentId: data.momentId,
