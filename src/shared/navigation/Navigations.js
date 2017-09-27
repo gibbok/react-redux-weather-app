d0102c33acde0e4a5a626fff67ff4a6674237d38
GibboK <gibbok.coding@gmail.com>
2017-09-27 11:06:30 +0200

    Add flow annotation for Navigations

--- src/shared/navigation/Navigations.js
+++ src/shared/navigation/Navigations.js
@@ -1,16 +1,32 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 import Navigation from './Navigation'
 
-const Navigations = ({ navigations, onNavigationClick }) => (
-  <div>
-    {navigations.map((navigation, index) =>
-      <Navigation
-        key={index}
-        {...navigations[index]}
-        onClick={() => onNavigationClick(navigation)}
+/* eslint-disable no-undef */
+type PropsType = {
+  +navigations: Array<{
+    id:string,
+    isActive:boolean,
+    title:string,
+    tooltip:string,
+    url:string
+  }>,
+  +onNavigationClick: Function,
+}
+/* eslint-enable no-undef */
+
+const Navigations:React.StatelessFunctionalComponent<any> = ({ navigations, onNavigationClick }:PropsType):React.Element<any> => {
+  return (
+    <div>
+      {navigations.map((navigation, index) =>
+        <Navigation
+          key={index}
+          {...navigations[index]}
+          onClick={() => onNavigationClick(navigation)}
       />
     )}
-  </div>
-)
+    </div>
+  )
+}
 
 export default Navigations
