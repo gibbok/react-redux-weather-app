890cfa3ef2686cdaff0cdb966c912b63a79fe0dc
GibboK <gibbok.coding@gmail.com>
2017-09-27 15:20:18 +0200

    Add and setup material-ui Includes adding in public robot font and MuiThemeProvider

--- src/app/Root.js
+++ src/app/Root.js
@@ -4,15 +4,18 @@ import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Forecast from './Forecast'
 import Map from './Map'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 const Root = ({ store } : { store : Object }) => (
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Forecast} />
-        <Route exact path='/map' component={Map} />
-      </Switch>
-    </Router>
+    <MuiThemeProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Forecast} />
+          <Route exact path='/map' component={Map} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   </Provider>
 )
 export default Root
