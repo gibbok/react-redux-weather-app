44939443ed08953de2fe5c50aaf660dd54bc9217
GibboK <gibbok.coding@gmail.com>
2017-09-27 10:57:53 +0200

    Add flow annotation for Navigation

--- src/shared/navigation/Navigation.js
+++ src/shared/navigation/Navigation.js
@@ -1,7 +1,18 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const Navigation = ({ onClick, id, title, tooltip, url }) => (
+/* eslint-disable no-undef */
+type PropsType = {
+  +onClick: Function,
+  +id: number,
+  +title:string,
+  +tooltip:string,
+  +url:string
+}
+/* eslint-enable no-undef */
+
+const Navigation:React.StatelessFunctionalComponent<any> = ({ onClick, id, title, tooltip, url }:PropsType):React.Element<any> => (
   <div onClick={onClick} alt={tooltip}>
     <NavLink
       to={url}
