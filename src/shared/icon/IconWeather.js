04701de8f42eee9a9aea5b65a8cb70e404c19ed6
GibboK <gibbok.coding@gmail.com>
2017-09-27 10:49:37 +0200

    Add flow annotation for IconWeather

--- src/shared/icon/IconWeather.js
+++ src/shared/icon/IconWeather.js
@@ -1,7 +1,15 @@
+
+// @flow
 import * as React from 'react'
 import getIconClassName from '../../app/weatherIcons'
 
-const IconWeather = ({code}:PropsType) => {
+/* eslint-disable no-undef */
+type PropsType = {
+  +code?: number,
+}
+/* eslint-enable no-undef */
+
+const IconWeather:React.StatelessFunctionalComponent<any> = ({code}:PropsType) => {
   if (code === undefined) {
     return null
   }
