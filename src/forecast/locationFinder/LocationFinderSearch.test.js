fc242ca16ad6e90e41cd6d6aefc035b341cf387a
GibboK <gibbok.coding@gmail.com>
2017-09-27 21:20:34 +0200

    Add test case for material-ui on LocationFinderSearch

--- src/forecast/locationFinder/LocationFinderSearch.test.js
+++ src/forecast/locationFinder/LocationFinderSearch.test.js
@@ -28,31 +28,17 @@ describe('<LocationFinderSearch />', () => {
     expect(onLocationChange.called).toEqual(true)
   })
 
-  // it('should simulate click event on material-ui FlatButton', () => {
-  //   const onSearchClick = sinon.spy()
-  //   const wrapper = shallow(<FlatButton onClick={onSearchClick} />)
-  //   console.log(wrapper.debug())
-  //   wrapper.find('FlatButton').dive().find('button').simulate('click')
-  //   expect(onSearchClick.called).toEqual(true)
-  // })
+  it('should simulate click event on material-ui FlatButton', () => {
+    const onSearchClick = sinon.spy()
+    const wrapper = shallow(<LocationFinderSearch onSearchClick={onSearchClick} />)
+    wrapper.find(FlatButton).simulate('click')
+    expect(onSearchClick.called).toEqual(true)
+  })
 
-  // it('should mount in a full DOM', () => {
-  //   expect(mount(<LocationFinderSearch />).find('form').length).toBe(1)
-  //   expect(mount(<LocationFinderSearch />).find('input[type=\'text\']').length).toBe(1)
-  //   expect(mount(<LocationFinderSearch />).find('input[type=\'submit\']').length).toBe(1)
-  // })
-
-  // it('should simulate change event on input of type text', () => {
-  //   const onLocationChange = sinon.spy()
-  //   const wrapper = shallow(<LocationFinderSearch onLocationChange={onLocationChange} />)
-  //   wrapper.find('input[type=\'text\']').simulate('change')
-  //   expect(onLocationChange.called).toEqual(true)
-  // })
-
-  // it('should simulate click event on input of type submit', () => {
-  //   const onSearchClick = sinon.spy()
-  //   const wrapper = shallow(<LocationFinderSearch onSearchClick={onSearchClick} />)
-  //   wrapper.find('input[type=\'submit\']').simulate('click')
-  //   expect(onSearchClick.called).toEqual(true)
-  // })
+  it('should simulate onKeyPress event on material-ui TextField', () => {
+    const onLocationKeyPress = sinon.spy()
+    const wrapper = shallow(<LocationFinderSearch onLocationKeyPress={onLocationKeyPress} />)
+    wrapper.find(TextField).simulate('keypress')
+    expect(onLocationKeyPress.called).toEqual(true)
+  })
 })
