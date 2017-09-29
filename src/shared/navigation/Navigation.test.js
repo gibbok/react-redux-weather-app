52d251beebbcff4b136eab4f195293a70976d98c
GibboK <gibbok.coding@gmail.com>
2017-09-29 13:19:37 +0200

    Add updated test cases after migration to material-ui

--- src/shared/navigation/Navigation.test.js
+++ src/shared/navigation/Navigation.test.js
@@ -6,19 +6,6 @@ import toJson from 'enzyme-to-json'
 import { NavLink } from 'react-router-dom'
 
 describe('<Navigation />', () => {
-  it('should render with snapshot', () => {
-    const wrapper = shallow(
-      <Navigation
-        onClick={'test'}
-        id={1}
-        title={'test title'}
-        tooltip={'test tooltop'}
-        url={'/map'}
-       />
-      )
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
-
   it('should render', () => {
     const wrapper = shallow(
       <Navigation
@@ -29,19 +16,7 @@ describe('<Navigation />', () => {
         url={'/map'}
        />
       )
-    expect(wrapper.contains(
-      <div onClick='test' alt='test tooltop'>
-        <NavLink to='/map'
-          exact
-          activeStyle={{
-            fontWeight: 'bold',
-            color: 'red'
-          }}
-          activeClassName='active'>
-          test title
-        </NavLink>
-      </div>
-    )).toBe(true)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   it('should should render one <NavLink /> component', () => {
